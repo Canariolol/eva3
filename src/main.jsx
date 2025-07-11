d7ca2b6f94afadab24a47bcaaaac0797aa3cfc44
rorro.prg <rorro.prg@gmail.com>
2025-07-11 04:13:31 +0000

    App.cs fue refactorizado.

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import './responsive.css';
-import 'react-quill/dist/quill.snow.css'; // Importar estilos de Quill
+import 'react-quill/dist/quill.snow.css';
+
+// Nuevos estilos refactorizados
+import './styles/layout.css';
+import './styles/sidebar.css';
+import './styles/forms.css';
+import './styles/cards.css';
+import './styles/components.css';
+import './styles/modal.css';
+import './styles/animations.css';
+
 import { GlobalProvider } from './context/GlobalContext';
 import { AuthProvider } from './context/AuthContext';
 import { BrowserRouter as Router } from 'react-router-dom';
