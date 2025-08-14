9f30a205879f11ecc1ebad355bd05bb7cb5ec737
rorro.prg <rorro.prg@gmail.com>
2025-08-14 14:49:57 +0000

    Porcenajes correctos para escalas binarias, en toda la pestaña de Equipo y sus modales.

--- src/components/Dashboard/CustomTooltip.jsx
+++ src/components/Dashboard/CustomTooltip.jsx
@@ -4,27 +4,28 @@ const CustomTooltip = ({ active, payload, scaleType }) => {
     if (active && payload && payload.length) {
         const barColor = payload[0].fill;
         const data = payload[0].payload;
-        const scaleType = data.scaleType || '1-10';
+        const resolvedScaleType = data.scaleType || '1-10';
         
-        let displayValue;
+        let displayValue = '';
         if (scaleType === 'binary') {
-            displayValue = `${payload[0].value.toFixed(1)}%`;
-        } else {
-            displayValue = payload[0].value.toFixed(2);
-        }
-        return (
-            <div className="recharts-custom-tooltip" style={{ border: `1px solid ${barColor}` }}>
-                <p className="recharts-tooltip-label" style={{ color: barColor, borderBottom: `1px solid ${barColor}50` }}>
-                    {payload[0].payload.name}
-                </p>
-                <p className="recharts-tooltip-intro" style={{ color: barColor }}>
-                    <span>{`${payload[0].name}: `}</span>
-                    <span style={{ fontWeight: 'bold' }}>{displayValue}</span>
-                </p>
-            </div>
-        );
-    }
-    return null;
+           displayValue = `${payload[0].value.toFixed(1)}%`;
+       } else {
+           // Numeric is a score 0-10, so we multiply by 10
+           displayValue = `${(payload[0].value * 1).toFixed(0)}`;
+       }
+       return (
+           <div className="recharts-custom-tooltip" style={{ border: `1px solid ${barColor}` }}>
+               <p className="recharts-tooltip-label" style={{ color: barColor, borderBottom: `1px solid ${barColor}50` }}>
+                   {payload[0].payload.name}
+               </p>
+               <p className="recharts-tooltip-intro" style={{ color: barColor }}>
+                   <span>{`${payload[0].name}: `}</span>
+                   <span style={{ fontWeight: 'bold' }}>{displayValue}</span>
+               </p>
+           </div>
+       );
+   }
+   return null;
 };
 
 export default CustomTooltip;
